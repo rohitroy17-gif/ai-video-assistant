44c03ef12567c8f77df6f2ccf2996cdcfbc49120
Rohit Roy <roy172758@gmail.com>
2026-09-17 18:16:51 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from audio_processor import process_input
 from transcriber import transcribe_all
 from summarizer import summarize, generate_title
-from extractor import extract_action_items, extract_key_decisions, extract_questions
+from meeting_extractor import extract_action_items, extract_key_decisions, extract_questions
 from rag_engine import build_rag_chain, ask_question
 
 load_dotenv()

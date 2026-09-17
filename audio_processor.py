c355724ce98c81aa1730d60693008460481d128e
Rohit Roy <roy172758@gmail.com>
2026-09-17 19:40:34 +0600

    Add files via upload

--- audio_processor.py
+++ audio_processor.py
@@ -10,6 +10,7 @@ def download_youtube_audio(url :str) ->str:
     ydl_opts = {
         "format": "bestaudio/best",
         "outtmpl": output_path,
+        "js_runtimes": {"node": {}},
         "postprocessors": [
             {
                 "key": "FFmpegExtractAudio",

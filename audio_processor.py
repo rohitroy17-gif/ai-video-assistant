34efe686dada95000ec1ccbb73278e363285fc2f
Rohit Roy <roy172758@gmail.com>
2026-09-17 17:37:13 +0600

    Add files via upload

--- audio_processor.py
+++ audio_processor.py
@@ -5,22 +5,32 @@ import os
 DOWNLOAD_DIR = "downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
-def download_youtube_video(url:str)-> str:
+def download_youtube_audio(url: str) -> str:
 
-    output_path = os.path.join(DOWNLOAD_DIR, '%(title)s.%(ext)s')
+    output_path = os.path.join(
+        DOWNLOAD_DIR,
+        "%(title)s.%(ext)s"
+    )
+
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': output_path,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-        }],
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "192",
+            }
+        ],
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
-        filename=ydl.prepare_filename(info).replace('.webm', '.wav').replace('.m4a', '.wav')
+
+        filename = ydl.prepare_filename(info)
+        filename = os.path.splitext(filename)[0] + ".wav"
+
         return filename
 
 
